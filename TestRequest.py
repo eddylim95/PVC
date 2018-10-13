7c222767cfac2e9ec7747f5f23c2993dc82b3836
eddylim95 <eddylim95@hotmail.com>
2018-10-14 01:27:02 +0800

    Update Test

--- TestRequest.py
+++ TestRequest.py
@@ -1,4 +1,7 @@
 import requests
 
 r = requests.post('http://127.0.0.1:5000/getJson', json = {'url': 'https://shopee.sg/-Bundle-of-4-Dynamo-Laundry-Detergent-Power-Gel-(2.7L-3L)-i.14134779.186143848'})
+
 print('Posted')
+
+print(r.json())
